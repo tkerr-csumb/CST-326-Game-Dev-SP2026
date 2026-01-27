16c780efcbb53fe2870f3a3a98ce8e171b92fa06
T_Kerr <tkerr@csumb.edu>
2026-01-26 21:22:14 -0800

    We didn't start the fire
    but ho boy it's burning

--- cst236_roll_da_ball/Assets/Scripts/PlayerController.cs
+++ cst236_roll_da_ball/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,17 +10,39 @@ public class PlayerController : MonoBehaviour
     private float movementX;
     private float movementY;
     public float speed;
+    public TextMeshProUGUI countText;
+    private int count;
+    public GameObject winTextObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
      rb = GetComponent<Rigidbody>();   
+     count = 0;
+     SetCountText();
+     winTextObject.SetActive(false);
     }
     void OnMove(InputValue movementValue){
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+    void SetCountText() 
+   {
+       countText.text =  "Polyhedrons: " + count.ToString();
+       if (count >= 12)
+       {
+           winTextObject.SetActive(true);
+       }
+   }
     void FixedUpdate(){
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("PickUp")){
+        other.gameObject.SetActive(false);
+        count = count +1;
+        SetCountText();
+       }
+   }
 }
