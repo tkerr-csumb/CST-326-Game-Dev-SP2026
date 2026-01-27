798e50e5bd75c7914fb68b1f5b4fe010dc03a5db
T_Kerr <tkerr@csumb.edu>
2026-01-26 20:00:53 -0800

    I feel like there should be more
    oh well!

---
+++ cst236_roll_da_ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+     rb = GetComponent<Rigidbody>();   
+    }
+    void OnMove(InputValue movementValue){
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void FixedUpdate(){
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+}
