1d49f7072118e8fe36761849533f872227d43f93
T_Kerr <tkerr@csumb.edu>
2026-02-04 20:30:02 -0800

    HorsePlinko Finalized

--- horse_plinko/Assets/Scripts/BallSpawner.cs
+++ horse_plinko/Assets/Scripts/BallSpawner.cs
@@ -4,15 +4,20 @@ using UnityEngine.InputSystem;
 public class BallSpawner : MonoBehaviour
 {
     public GameObject horsePrefab;
+    public float spawnerDistance;
+    public float spawnerSpeed;
+    private Vector3 startSpot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(horsePrefab);
+        startSpot = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float xOffset = Mathf.Sin(Time.time * spawnerSpeed * 2f * Mathf.PI) * spawnerDistance;
+        transform.position = startSpot + new Vector3(xOffset, 0f, 0f);
         if(Keyboard.current.spaceKey.isPressed)
         {
             Transform myTransform = GetComponent<Transform>();
