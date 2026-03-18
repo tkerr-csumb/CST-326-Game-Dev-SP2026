715b47469eeefb46f353f9ef0407ccf46f41e3bd
T_Kerr <tkerr@csumb.edu>
2026-03-18 01:47:48 -0700

    This
    i'm not gonna lie, this dude goes WAY too fast and then also there's so many tangents it's hard to follow

--- codeMonkeyTutorial/Assets/Scripts/GameInput.cs
+++ codeMonkeyTutorial/Assets/Scripts/GameInput.cs
@@ -1,24 +1,23 @@
 using UnityEngine;
+using System;
 
 public class GameInput : MonoBehaviour
 {
-    public Vector2 GetMovementVectorNormalized(){
-        
-        Vector2 inputVector = new Vector2(0,0);
-        
-        if (Input.GetKey(KeyCode.W)){
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)){
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)){
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)){
-            inputVector.x = +1;
-        }
+    public event EventHandler OnInteractAction;
+    private PlayerInputActions playerInputActions;
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
 
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj){
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Vector2 GetMovementVectorNormalized(){
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
         return inputVector;
     }
--- codeMonkeyTutorial/Assets/Scripts/Player.cs
+++ codeMonkeyTutorial/Assets/Scripts/Player.cs
@@ -1,22 +1,114 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
 
+    public static Player Instance {get; private set;}
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
+    private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+    private void Awake(){
+        if (Instance != null){
+            Debug.LogError("There is more than one Player instance.");
+        }
+        Instance = this;
+    }
+
+    private void Start(){
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e){
+        if (selectedCounter != null)
+        {
+            selectedCounter.Interact();
+        }
+    }
+
     private void Update(){
+       HandleMovement();
+       HandleInteractions();
+    }
+
+    public bool IsWalking(){
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        float interactDistance = 2f;
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        if (moveDir != Vector3.zero){
+            lastInteractDir = moveDir;
+        }
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)){
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                // Has ClearCounter
+                if (clearCounter != selectedCounter){
+                    SetSelectedCounter(clearCounter);
+                }
+            } else {
+                SetSelectedCounter(null);
+            }
+        } else {
+            SetSelectedCounter(null);
+        }
+    }
+
+    private void HandleMovement(){
+         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position+Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove){
+            //Cannot move towards moveDir
+
+            //Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position+Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove){
+                moveDir = moveDirX;
+            } else {
+                //Cannot move only on the X
+
+                //Attempt only Z movement
+                Vector3 moveDirZ = new Vector3 (0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position+Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove){
+                    moveDir = moveDirZ;
+                } else {
+                    //Cannot move in any direction
+                }
+            }
+        }
+        if (canMove){
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
         isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
-    public bool IsWalking(){
-        return isWalking;
+    private void SetSelectedCounter(ClearCounter selectedCounter){
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs{
+            selectedCounter = selectedCounter
+        });
     }
 }
